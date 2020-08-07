82826954db3c57dafd40f91c6552209cabbd3192
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2020-08-07 11:13:14 -0400

    Finished model and made server

--- model.py
+++ model.py
@@ -1,6 +1,26 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+'''
+Test with / make requests with 
+Note it is actually order sensative
+curl -i -H "Content-Type: application/json" -X POST -d '{"age":50, "high_risk_exposure_occupation": false, "labored_respiration": true, "wheezes": true, "cough": false, "temperature": 35, "diarrhea": false,"fatigue": false, "headache": true, "loss_of_smell": false, "loss_of_taste": true, "runny_nose": false, "muscle_sore": true, "sore_throat": true}' http://localhost:5000
+'''
+@app.route('/', methods = ["POST"])
+def hello_world():
+    data = request.json
+    for key in data:
+        data[key] = [data[key]]
+        
+
+    return jsonify({"prob": str(predict(pd.DataFrame.from_dict(data)))})
 
 class customScaler:
     def __init__(self, X_train):
@@ -14,23 +34,36 @@ class customScaler:
         data = data.replace({False: 0, True: 1})
         data = pd.concat([data.reset_index(drop = True), transformed], axis = 1)
         return data
-        
-def main():
-    data = pd.read_csv("data/data.csv")
-    data = data.drop(data.columns[0], axis = 1)
-    X, y = data.drop("covid19_test_results", axis=1), data[["covid19_test_results"]]
-    y = y.replace({"Negative": 0, "Positive": 1})
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42)
-
-    scaler = customScaler(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    print(X_test)
+def predict(row):
+    return model.predict_proba(scaler.transform(row))
     
-if __name__ == "__main__":
-    main()
+#def main():    
+data = pd.read_csv("data/data.csv")
+data = data.drop(data.columns[0], axis = 1)
+X, y = data.drop("covid19_test_results", axis=1), data[["covid19_test_results"]]
+y = y.replace({"Negative": 0, "Positive": 1})
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42)
+    
+scaler = customScaler(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = LogisticRegression(random_state = 42).fit(X_train, y_train)
+
+acc = model.score(X_test, y_test)
+preds = model.predict(X_test)
+cm = confusion_matrix(y_test, preds)
+print("Model trained with acc of ", acc)
+print("Confusion matrix of ")
+print(cm)
+
+app.run()
+    
+    
+#if __name__ == "__main__":
+ #   main()
 
 
 
